2f04c3418e6b2cddee2ea8e6d3f978f8b9d2061e
XuJun <nankaimathxujun@gmail.com>
2017-05-28 08:15:16 +0800

    add

--- NSC NIPS2002/NSC.m
+++ NSC NIPS2002/NSC.m
@@ -3,8 +3,8 @@ function [id]= NSC(B,y,Dlabels)
 % DSRIC classification function
 for c = 1:max(Dlabels)
     coef_c = B{c}*y;
-        error(c) = norm(y-coef_c,2)^2; % inferior 
-%     error(c) = norm(y-coef_c,2)^2/sum(coef_c.*coef_c);
+%         error(c) = norm(y-coef_c,2)^2; % inferior 
+    error(c) = norm(y-coef_c,2)^2/sum(coef_c.*coef_c);
 end
 
 index      =  find(error==min(error));
