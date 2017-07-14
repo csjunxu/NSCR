808231fc65b407165fbda2a95e3577b1cd6fac03
XuJun <nankaimathxujun@gmail.com>
2017-07-14 14:27:46 +0800

    add

--- NNCRC/Demo_Digit.m
+++ NNCRC/Demo_Digit.m
@@ -18,9 +18,9 @@ elseif strcmp(dataset, 'USPS') == 1
 end
 % -------------------------------------------------------------------------
 %% choosing classification methods
-ClassificationMethod = 'SRC'; addpath(genpath('l1_ls_matlab'));
+% ClassificationMethod = 'SRC'; addpath(genpath('l1_ls_matlab'));
 % ClassificationMethod = 'CRC';
-% ClassificationMethod = 'NNLSR' ; % non-negative LSR
+ClassificationMethod = 'NNLSR' ; % non-negative LSR
 % ClassificationMethod = 'NPLSR' ; % non-positive LSR
 % ClassificationMethod = 'ANNLSR' ; % affine and non-negative LSR
 % ClassificationMethod = 'ANPLSR' ; % affine and non-positive LSR
@@ -36,56 +36,45 @@ end
 %% Settings
 Par.nDim = 500;
 
-for nSample = [50 100 300 600] % number of images for each digit
+for nSample = [50 100 300 500] % number of images for each digit
     %-------------------------------------------------------------------------
     %% tuning the parameters
     for s = [1]
         Par.s = s;
         for maxIter = [5]
             Par.maxIter  = maxIter;
-            for rho = [1]
+            for rho = [.1 1 10]
                 Par.rho = rho*10^(-1);
-                for lambda = [.1 1 10]
-                    Par.lambda = lambda*10^(-1);
+                for lambda = [0]
+                    Par.lambda = lambda*10^(-2);
                     accuracy = zeros(nExperiment, 1) ;
                     for i = 1:nExperiment
-                        
                         %--------------------------------------------------------------------------
                         %% data loading
-                        if strcmp(dataset, 'MNIST2k2k') == 1
-                            load(['C:/Users/csjunxu/Desktop/Classification/Dataset/MNIST2k2k']);
-                            gnd = gnd + 1;
-                            Par.nClass        =   max(gnd); % the number of classes in the subset of AR database
-                            %                         Par.nDim          =   100; % the eigenfaces dimension
-                            Tr_DAT   =   double(fea(trainIdx, :))';
-                            trls     =   gnd(trainIdx, 1)';
-                            Tt_DAT   =   double(fea(testIdx, :))';
-                            ttls     =   gnd(testIdx, 1)';
-                            clear fea gnd trainIdx testIdx
-                        elseif strcmp(dataset, 'MNIST') == 1
+                        if strcmp(dataset, 'MNIST') == 1
                             %% Load data
                             addpath('C:\Users\csjunxu\Desktop\SC\Datasets\MNIST\')
-                            if ~exist('MNIST_DATA', 'var') || ~exist('tt_MNIST_DATA', 'var')
+                            if ~exist('tr_MNIST_DATA', 'var') || ~exist('tt_MNIST_DATA', 'var')
                                 try
                                     % MNIST_SC_DATA is a D by N matrix. Each column contains a feature
                                     % vector of a digit image and N = 60,000.
                                     % MNIST_LABEL is a 1 by N vector. Each entry is the label for the
                                     % corresponding column in MNIST_SC_DATA.
-                                    load MNIST_C.mat MNIST_SC_DATA MNIST_LABEL;
+                                    load tr_MNIST_C.mat tr_MNIST_C_DATA tr_MNIST_LABEL;
+                                    load tt_MNIST_C.mat tt_MNIST_C_DATA tt_MNIST_LABEL;
                                 catch
                                     %                                     % training data
                                     %                                     tr_MNIST_DATA = loadMNISTImages('train-images.idx3-ubyte');
                                     %                                     tr_MNIST_LABEL = loadMNISTLabels('train-labels.idx1-ubyte');
                                     %                                     tr_MNIST_C_DATA = SCofDigits(tr_MNIST_DATA);
-                                    %                                     save C:\Users\csjunxu\Desktop\SC\Datasets\tr_MNIST_C.mat tr_MNIST_C_DATA tr_MNIST_LABEL;
+                                    %                                     save C:\Users\csjunxu\Desktop\SC\Datasets\MNIST\tr_MNIST_C.mat tr_MNIST_C_DATA tr_MNIST_LABEL;
                                     % testing data
                                     tt_MNIST_DATA = loadMNISTImages('t10k-images.idx3-ubyte');
                                     tt_MNIST_LABEL = loadMNISTLabels('t10k-labels.idx1-ubyte');
                                     tt_MNIST_C_DATA = SCofDigits(tt_MNIST_DATA);
-                                    save C:\Users\csjunxu\Desktop\SC\Datasets\tt_MNIST_C.mat tt_MNIST_C_DATA tt_MNIST_LABEL;
+                                    save C:\Users\csjunxu\Desktop\SC\Datasets\MNIST\tt_MNIST_C.mat tt_MNIST_C_DATA tt_MNIST_LABEL;
                                 end
-                                tr_MNIST_DATA = MNIST_SC_DATA ;% tr_MNIST_C_DATA;
-                                tr_MNIST_LABEL = MNIST_LABEL;
+                                tr_MNIST_DATA = tr_MNIST_C_DATA;
                                 tt_MNIST_DATA = tt_MNIST_C_DATA;
                             end
                             nCluster = 10;
@@ -107,7 +96,6 @@ for nSample = [50 100 300 600] % number of images for each digit
                                 allpos = find( tr_MNIST_LABEL == Digits(iK) );
                                 rng( (i-1) * nCluster + iK );
                                 selpos = allpos( randperm(length(allpos), nSample(iK)) );
-                                
                                 mask( nSample_cum(iK) + 1 : nSample_cum(iK+1) ) = selpos;
                                 gnd( nSample_cum(iK) + 1 : nSample_cum(iK+1) ) = iK * ones(1, nSample(iK));
                             end
@@ -115,19 +103,15 @@ for nSample = [50 100 300 600] % number of images for each digit
                             Tr_DAT = tr_MNIST_DATA(:, mask);
                             trls = gnd;
                             Tt_DAT   =   tt_MNIST_DATA;
-                            ttls     =   tt_MNIST_LABEL;
+                            ttls     =   tt_MNIST_LABEL' + 1;
                         elseif strcmp(dataset, 'USPS')==1
-                            load('../USPS');
-                            par.nClass        =  length(unique(gnd));
+                            load('C:\Users\csjunxu\Desktop\SC\Datasets\USPS');
                             Tr_DAT   =   double(fea(1:7291, :)');
                             trls     =   gnd(1:7291)';
                             Tt_DAT   =   double(fea(7292:end, :)');
                             ttls     =   gnd(7292:end)';
-                            Tr_DAT  =  Tr_DAT./( repmat(sqrt(sum(Tr_DAT.*Tr_DAT)), [size(Tr_DAT, 1), 1]) );
-                            Tt_DAT  =  Tt_DAT./( repmat(sqrt(sum(Tt_DAT.*Tt_DAT)), [size(Tt_DAT, 1), 1]) );
                             clear fea gnd
                         end
-                        
                         %--------------------------------------------------------------------------
                         %% eigenface extracting
                         [disc_set,disc_value,Mean_Image]  =  Eigenface_f(Tr_DAT, Par.nDim);
@@ -135,7 +119,6 @@ for nSample = [50 100 300 600] % number of images for each digit
                         tt_dat  =  disc_set'*Tt_DAT;
                         tr_dat  =  tr_dat./( repmat(sqrt(sum(tr_dat.*tr_dat)), [Par.nDim,1]) );
                         tt_dat  =  tt_dat./( repmat(sqrt(sum(tt_dat.*tt_dat)), [Par.nDim,1]) );
-                        
                         %-------------------------------------------------------------------------
                         %% testing
                         ID = [];
@@ -177,16 +160,15 @@ for nSample = [50 100 300 600] % number of images for each digit
                         accuracy(i, 1)         =   [cornum/length(ttls)]; % recognition rate
                         fprintf(['Accuracy is ' num2str(accuracy(i, 1)) '.\n']);
                     end
-                    
                     % -------------------------------------------------------------------------
                     %% save the results
                     avgacc = mean(accuracy);
                     fprintf(['Mean Accuracy is ' num2str(avgacc) '.\n']);
-                    if strcmp(ClassificationMethod, 'CRC') == 1 % strcmp(ClassificationMethod, 'SRC') == 1 ||
-                        matname = sprintf([writefilepath dataset '_' ClassificationMethod '_DR' num2str(Par.nDim) '.mat']);
+                    if strcmp(ClassificationMethod, 'SRC') == 1 || strcmp(ClassificationMethod, 'CRC') == 1
+                        matname = sprintf([writefilepath dataset '_' num2str(nSample(1)) '_' num2str(nExperiment) '_' ClassificationMethod '_DR' num2str(Par.nDim) '_lambda' num2str(Par.lambda) '.mat']);
                         save(matname, 'accuracy', 'avgacc');
                     else
-                        matname = sprintf([writefilepath dataset '_' ClassificationMethod '_DR' num2str(Par.nDim) '_scale' num2str(Par.s) '_maxIter' num2str(Par.maxIter) '_rho' num2str(Par.rho) '_lambda' num2str(Par.lambda) '.mat']);
+                        matname = sprintf([writefilepath dataset '_' num2str(nSample(1)) '_' num2str(nExperiment) '_' ClassificationMethod '_DR' num2str(Par.nDim) '_scale' num2str(Par.s) '_maxIter' num2str(Par.maxIter) '_rho' num2str(Par.rho) '_lambda' num2str(Par.lambda) '.mat']);
                         save(matname,'accuracy', 'avgacc');
                     end
                 end
