8369237fbb64ac948b476140c1b1431c8601d3cf
XuJun <nankaimathxujun@gmail.com>
2017-08-25 15:15:27 +0800

    add

--- NNLS/Demo_ObjectActionFineGrained2.m
+++ NNLS/Demo_ObjectActionFineGrained2.m
@@ -43,9 +43,9 @@ if ~isdir(writefilepath)
 end
 % -------------------------------------------------------------------------
 %% choosing classification methods
-% ClassificationMethod = 'NSC';
+ClassificationMethod = 'NSC';
 % ClassificationMethod = 'SRC'; addpath(genpath('C:\Users\csjunxu\Desktop\Classification\l1_ls_matlab'));
-ClassificationMethod = 'CRC';
+% ClassificationMethod = 'CRC';
 % ClassificationMethod = 'CROC'; addpath(genpath('C:\Users\csjunxu\Desktop\Classification\CROC CVPR2012'));
 % ClassificationMethod = 'ProCRC'; addpath(genpath('C:\Users\csjunxu\Desktop\Classification\ProCRC'));
 % ClassificationMethod = 'NNLSR' ; % non-negative LSR
