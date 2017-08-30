a9571fa2080886851b39325dd658e80b3474d31d
XuJun <nankaimathxujun@gmail.com>
2017-08-30 09:06:26 +0800

    add

--- NNLS/Demo_ObjectActionFineGrained.m
+++ NNLS/Demo_ObjectActionFineGrained.m
@@ -14,7 +14,7 @@ if strcmp(dataset, 'CUB-200-2011_VGG') == 1
     nExperiment = 1;
     nDimArray = [4096];
 elseif strcmp(dataset, 'Flower-102_VGG') == 1
-    v = 1;
+    nExperiment = 1;
     nDimArray = [4096];
 elseif strcmp(dataset, 'Standford-40_VGG') == 1
     nExperiment = 1;
@@ -38,10 +38,10 @@ end
 %% choosing classification methods
 % ClassificationMethod = 'NSC';
 % ClassificationMethod = 'SRC'; addpath(genpath('C:\Users\csjunxu\Desktop\Classification\l1_ls_matlab'));
-ClassificationMethod = 'CRC';
+% ClassificationMethod = 'CRC';
 % ClassificationMethod = 'CROC'; addpath(genpath('C:\Users\csjunxu\Desktop\Classification\CROC CVPR2012'));
 % ClassificationMethod = 'ProCRC'; addpath(genpath('C:\Users\csjunxu\Desktop\Classification\ProCRC'));
-% ClassificationMethod = 'NNLSR' ; % non-negative LSR
+ClassificationMethod = 'NNLSR' ; % non-negative LSR
 % ClassificationMethod = 'NPLSR' ; % non-positive LSR
 % ClassificationMethod = 'ANNLSR' ; % affine and non-negative LSR
 % ClassificationMethod = 'ANPLSR' ; % affine and non-positive LSR
@@ -58,9 +58,9 @@ for nDim = nDimArray
         %% tuning the parameters
         for s = [1]
             Par.s = s;
-            for maxIter = [5]
+            for maxIter = [10 15]
                 Par.maxIter  = maxIter;
-                for rho = [.1]
+                for rho = [1]
                     Par.rho = rho;
                     for lambda = [0]
                         Par.lambda = lambda;
