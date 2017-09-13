d9605364260f6a43390093ed71eb0cdb71000b83
XuJun <nankaimathxujun@gmail.com>
2017-09-13 21:12:21 +0800

    a

--- NNLS/Demo_face.m
+++ NNLS/Demo_face.m
@@ -48,9 +48,9 @@ for nDim = nDimArray
     %% tuning the parameters
     for s = [1]
         Par.s = s;
-        for maxIter = [5:1:10]
+        for maxIter = [9:1:10]
             Par.maxIter  = maxIter;
-            for rho = [.2:.2:.8]
+            for rho = [.1:.1:.4]
                 Par.rho = rho;
                 for lambda = [0]
                     Par.lambda = lambda;
