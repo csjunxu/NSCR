832531b53b71d47d8b1dba464cf642f7a4958fae
XuJun <nankaimathxujun@gmail.com>
2017-10-13 08:37:36 +0800

    a

--- NNLS/Demo_ObjectActionFineGrained0.m
+++ NNLS/Demo_ObjectActionFineGrained0.m
@@ -69,9 +69,9 @@ for nDim = nDimArray
         %% tuning the parameters
         for s = [1]
             Par.s = s;
-            for maxIter = [1:1:3]
+            for maxIter = [1:1:5]
                 Par.maxIter  = maxIter;
-                for rho = [1:.5:3]
+                for rho = [ .9 1.1 .8 1.2]
                     Par.rho = rho;
                     for lambda = [0]
                         Par.lambda = lambda;
