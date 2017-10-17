142bb5c98aa960553dc5aef0371d4eef336fd1c3
XuJun <nankaimathxujun@gmail.com>
2017-10-17 15:37:33 +0800

    a

--- NNLS/Demo_LargeDataset_NNLS.m
+++ NNLS/Demo_LargeDataset_NNLS.m
@@ -69,9 +69,9 @@ for nDim = nDimArray
         %% tuning the parameters
         for s = [1]
             Par.s = s;
-            for maxIter = [3:1:10]
+            for maxIter = [4:1:10]
                 Par.maxIter  = maxIter;
-                for rho = [.01 .05 .1:.1:1]
+                for rho = [.05 .1:.1:1]
                     Par.rho = rho;
                     for lambda = [0]
                         Par.lambda = lambda;
