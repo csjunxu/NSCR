ecd96c47f16a82d3017428de759bb0018ab546e4
XuJun <nankaimathxujun@gmail.com>
2017-10-17 20:33:51 +0800

    a

--- NNLS/Demo_ImageNet_NNLS1.m
+++ NNLS/Demo_ImageNet_NNLS1.m
@@ -39,9 +39,9 @@ for nDim = nDimArray
         %% tuning the parameters
         for s = [1]
             Par.s = s;
-            for maxIter = [5:1:10]
+            for maxIter = [1:2:9]
                 Par.maxIter  = maxIter;
-                for rho = [.3:.1:1]
+                for rho = [.1:.1:1]
                     Par.rho = rho;
                     for lambda = [0]
                         Par.lambda = lambda;
