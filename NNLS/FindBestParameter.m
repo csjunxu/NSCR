1baf6068a35c6dc796526041d12d347cc8bf68f5
XuJun <nankaimathxujun@gmail.com>
2018-02-08 15:26:01 +0800

    add

--- NNLS/FindBestParameter.m
+++ NNLS/FindBestParameter.m
@@ -14,11 +14,7 @@ Acc = [];
 for i = 1:im_num
     result = fullfile(Original_image_dir, im_dir(i).name);
     eval(['load ' num2str(result)]);
-    Y1 = [Y1 mPSNR(end)];
-    Y2 = [Y2 mSSIM(end)];
     Acc = [Acc accuracy];
 end
-createfigure(X1, Y1, Y2);
 maxAcc = max(Acc);
- fprintf('%s : maxAcc = %2.4f: ', im_dir(i).name, maxAcc);
-   
+fprintf('%s : maxAcc = %2.4f: ', im_dir(i).name, maxAcc);
