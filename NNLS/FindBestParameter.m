0981bbb4eb0358ebd5b7791f38652ed71cfc31ba
XuJun <nankaimathxujun@gmail.com>
2018-02-08 17:05:46 +0800

    add

--- NNLS/FindBestParameter.m
+++ NNLS/FindBestParameter.m
@@ -1,12 +1,12 @@
 
 clear
-dataset = 'AR_DAT';
+dataset = 'YaleBCrop025';
 % AR_DAT
 % YaleBCrop025
 %% directory to save the results
-Original_image_dir  = ['C:/Users/csjunxu/Desktop/Classification/Results/' dataset '/'];
+Original_image_dir  = ['C:/Users/csjunxu/Desktop/DANNLSR/Results/' dataset '/'];
 Sdir = regexp(Original_image_dir, '/', 'split');
-fpath = fullfile(Original_image_dir, 'AR_DAT_DANNLSR_DR54*.mat');
+fpath = fullfile(Original_image_dir, '*.mat');
 im_dir  = dir(fpath);
 im_num = length(im_dir);
 
@@ -14,7 +14,10 @@ Acc = [];
 for i = 1:im_num
     result = fullfile(Original_image_dir, im_dir(i).name);
     eval(['load ' num2str(result)]);
-    Acc = [Acc accuracy];
+    if avgacc >= 0.984
+        fprintf('%s;\n ', im_dir(i).name);
+    end
+    Acc = [Acc avgacc];
 end
-maxAcc = max(Acc);
-fprintf('%s : maxAcc = %2.4f: ', im_dir(i).name, maxAcc);
+[maxAcc, id] = max(Acc);
+fprintf('%s : maxAcc = %2.4f: ', im_dir(id).name, maxAcc);
